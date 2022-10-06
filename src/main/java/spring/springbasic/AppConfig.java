5ccc561c8e6b56c6adfbc3442fd011b797a78e59
kimbro97 <rlagudwog@gmail.com>
2022-10-06 09:06:51 +0900

    스프링으로 전환하기

--- src/main/java/spring/springbasic/AppConfig.java
+++ src/main/java/spring/springbasic/AppConfig.java
@@ -1,7 +1,8 @@
 package spring.springbasic;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import spring.springbasic.discount.DiscountPolicy;
-import spring.springbasic.discount.FixDiscountPolicy;
 import spring.springbasic.discount.RateDiscountPolicy;
 import spring.springbasic.member.MemberRepository;
 import spring.springbasic.member.MemberService;
@@ -10,19 +11,24 @@ import spring.springbasic.member.MemoryMemberRepository;
 import spring.springbasic.order.OrderService;
 import spring.springbasic.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
--- src/main/java/spring/springbasic/MemberApp.java
+++ src/main/java/spring/springbasic/MemberApp.java
@@ -1,18 +1,22 @@
 package spring.springbasic;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.springbasic.member.Grade;
 import spring.springbasic.member.Member;
 import spring.springbasic.member.MemberService;
 import spring.springbasic.member.MemberServiceImpl;
 
 public class MemberApp {
-//    public static void main(String[] args) {
-//        MemberService memberService = new MemberServiceImpl();
-//        Member member = new Member(1L, "memberA", Grade.VIP);
-//        memberService.join(member);
-//
-//        Member findMember = memberService.findMember(1L);
-//        System.out.println("findMember = " + findMember.getName());
-//        System.out.println("member = " + member.getName());
-//    }
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService =  applicationContext.getBean("memberService", MemberService.class);
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("findMember = " + findMember.getName());
+        System.out.println("member = " + member.getName());
+    }
 }
