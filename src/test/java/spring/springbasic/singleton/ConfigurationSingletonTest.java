6a38febd2a1d423661b8acb841faed3c66eeabb5
kimbro97 <rlagudwog@gmail.com>
2022-10-10 12:58:03 +0900

    @Configuration과 바이트코드 조작의 마법

---
+++ src/test/java/spring/springbasic/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,28 @@
+package spring.springbasic.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.springbasic.AppConfig;
+import spring.springbasic.member.MemberRepository;
+import spring.springbasic.member.MemberServiceImpl;
+import spring.springbasic.order.OrderServiceImpl;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConfigurationSingletonTest {
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+        System.out.println("memberService -> memberRepository = " +memberService.getMemberRepository());
+        System.out.println("orderService -> memberRepository  = " + orderService.getMemberRepository());
+        System.out.println("memberRepository = " + memberRepository);
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+}
