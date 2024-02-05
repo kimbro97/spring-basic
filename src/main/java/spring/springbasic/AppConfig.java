53515bde6887b1a558237e39ecaead79651f2b40
kimbro97 <rlagudwog@gmail.com>
2024-02-05 22:21:06 +0900

    feat: t스프링으로 전환하기

--- src/main/java/spring/springbasic/AppConfig.java
+++ src/main/java/spring/springbasic/AppConfig.java
@@ -1,5 +1,7 @@
 package spring.springbasic;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import spring.springbasic.discount.DiscountPolicy;
 import spring.springbasic.discount.RateDiscountPolicy;
 import spring.springbasic.member.MemberRepository;
@@ -9,19 +11,21 @@ import spring.springbasic.member.MemoryMemberRepository;
 import spring.springbasic.order.OrderService;
 import spring.springbasic.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
-
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
--- src/main/java/spring/springbasic/MemberApp.java
+++ src/main/java/spring/springbasic/MemberApp.java
@@ -1,14 +1,17 @@
 package spring.springbasic;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.springbasic.member.Grade;
 import spring.springbasic.member.Member;
 import spring.springbasic.member.MemberService;
-import spring.springbasic.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         Member kimbro97 = new Member(1L, "kimbro97", Grade.VIP);
         memberService.join(kimbro97);
         Member findMember = memberService.findMember(1L);
