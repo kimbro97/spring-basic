231ee75e8daf1db602e6a730d1ff7db4424bcd81
kimbro97 <rlagudwog@gmail.com>
2024-02-05 22:00:02 +0900

    feat: AppConfig 리팩토링

--- src/main/java/spring/springbasic/AppConfig.java
+++ src/main/java/spring/springbasic/AppConfig.java
@@ -9,10 +9,18 @@ import spring.springbasic.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private static RateDiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 }
