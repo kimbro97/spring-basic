d64033ed41e2cb491341f291ff20d027525e5fc7
kimbro97 <rlagudwog@gmail.com>
2024-02-06 21:32:22 +0900

    feat: @Configuration과 싱글톤

---
+++ src/test/java/spring/springbasic/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,28 @@
+package spring.springbasic.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.springbasic.AppConfig;
+import spring.springbasic.member.MemberRepository;
+import spring.springbasic.member.MemberServiceImpl;
+import spring.springbasic.order.OrderServiceImpl;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+        System.out.println("memberRepository = " + memberRepository);
+        System.out.println("memberRepository1 = " + memberRepository1);
+        System.out.println("memberRepository2 = " + memberRepository2);
+        Assertions.assertThat(memberRepository1).isSameAs(memberRepository2);
+    }
+}
